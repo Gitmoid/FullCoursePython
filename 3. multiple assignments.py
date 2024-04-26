5553b7d429c4cb6e29dc6daa87f4b13dad311e32
Gitmoid <484635@mail.muni.cz>
2024-04-26 16:42:49 +0200

    Python full course

--- 3. multiple assignments.py
+++
@@ -1,9 +0,0 @@
-# name = "Bro"
-# age = 21
-# attractive = True
-
-name, age, attractive = "Bro", 21, True
-
-print(name)
-print(age)
-print(attractive)
--- 6. user input.py
+++
@@ -1,8 +0,0 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-height = float(input("Enter your height: "))
-
-age = age + 1
-
-print("Hello", name + ". You will see your age of", age, ".")
-print("Hello " + name + ". Your age is " + str(age) + "." + " Your height is " + str(height) + " cm.")
